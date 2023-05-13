c9b4795ef6131a5d2f4ed7199e619f6b708131d4
gustavomgs <53432968+gustavomgs@users.noreply.github.com>
2023-05-13 10:22:28 -0300

    Update codigo.java

--- codigo.java
+++ codigo.java
@@ -1,82 +1,65 @@
 import java.util.Scanner;
+import java.io.IOException;
 
-class ContaBancaria {
-    
-  static double saldo;  
-    
-    
-  public static void depositar(double valor) {
-      
-      saldo = saldo + valor;
-        
-      menu();
-  }
-  
-  public static double sacar(double valor) {
-      
-      saldo = saldo + valor;
-        
-    return saldo;
-  } 
-  
-  public static double exibirSaldo() {
-      
-    System.out.print(saldo);
-        
-    return saldo;
-  }
-  
-  public static void menu() {
-    Scanner teclado = new Scanner(System.in);   
+//Crie um programinha em java que leia 
+//3 valores que representam os lados de um triângulo;
+//Verifique se é um 
+//triângulo equilatero (três lados iguais), 
+//isósceles (dois lados iguais) 
+//escaleno (três lados diferentes). 
 
-    int opcao;
-    
-    System.out.print("Menu do meu sistema\n\n");
-    System.out.print("------------------------\n\n");
-    System.out.print("Por favor selecione uma opcao:\n");
-    System.out.print("------------------------\n\n");
-    System.out.print("[1] - Depositar\n");
-    System.out.print("[2] - Sacar\n");
-    System.out.print("[3] - Exibir saldo\n");
-    System.out.print("[4] - Sair do sistema\n");
-    
-    opcao = teclado.nextInt();       
-    
-    switch (opcao) {
-      case 1:
-      depositar(10.0);
-      break;
+//IF
+//AND (&&)
+//OR (||)
+//IGUAL ==
+//DIFERENTE !=
 
-      case 2:
-      sacar(10.0);
-      break;
 
-      case 3:
-      exibirSaldo();
-      break;
+class Console {
 
-      case 4:
-      System.exit(0);
-      break;
+    public static void clear(String... arg) throws IOException, InterruptedException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+    }
+}
 
-    default:
-      System.out.printf("Insira uma opção válida");
+class DescobrirTriangulo {
+
+	public static void main(String args[]) throws IOException, InterruptedException{  
+
+    Scanner teclado = new Scanner(System.in);       
+    Console console = new Console();    
+
+    int x = 2;
+    int y = 4;
+    int z = 3;
+
+    String tipo       = "Isosceles";
+
+    int continuar;   
+
+    if(x == y && x == z){
+      tipo = "Equilatero"; 
+    }
+
+    if(x != y && x != z){
+      tipo = "Escaleno";
+    }
+
+    console.clear();
+
+    System.out.printf("triangulo %s\n\n\n\n\n (: \n\n\n", tipo);
+
+    System.out.printf("Deseja inserir outro triangulo? ( 1 - Para sim | 2 - para nao)\n");
+
+    continuar = teclado.nextInt();
+
+    if(continuar == 1){
       main(null);
     }
-    
-  }
-  
 
-	public static void main(String args[]){  
-
-    saldo = 0;
-
-    menu();    
-    
-    System.out.print(saldo);
-    
+    console.clear();
     System.exit(0);
-		
+
 	}
 
 }
